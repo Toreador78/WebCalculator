1309d86e21a3de44a93b781d914118ee05768670
Oliver Osterbrink <oliver.osterbrink@googlemail.com>
2016-02-17 09:17:52 +0100

    Provides skeleton js
    main.js:
    - Skeleton main.js with click-functions giving a console log
    index.html:
    - Some changes in index.html are made. Mainly button classes where changed
    and fixed
    - CDN Link to jQuerry insert
    - Also Bootstrap is excluded, since it is not used

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  var textbox = $('.textbox');
+
+  console.log("ready)")
+
+  $(".number").click(function(){
+    console.log("number");
+  });
+  $(".basiccalc").click(function(){
+    console.log("basiccalc");
+  });
+  $(".AC").click(function(){
+    console.log("AC");
+  });
+  $(".PM").click(function(){
+    console.log("PM");
+  });
+  $(".decimal").click(function(){
+    console.log("decimal");
+  });
+  $(".eval").click(function(){
+    console.log("eval");
+  });
+});
