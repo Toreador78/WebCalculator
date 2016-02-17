ba495f1fbd619176d7438fcc36ec5129114920e3
Oliver Osterbrink <oliver.osterbrink@googlemail.com>
2016-02-17 09:55:46 +0100

    Implements basic functionality
    - Basic operations are implemented
    - Sequential evaluation
    - +/- button (Inverter) is not implemented yet

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,68 @@
 $(document).ready(function(){
   var textbox = $('.textbox');
 
-  console.log("ready)")
+  textbox.curval = "";
+
+  function checkDecimal(){
+    if (textbox.curval.indexOf(".") == -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function resetCalc(){
+    textbox.lastval = "";
+    textbox.curval = "0";
+    textbox.operation = "";
+    textbox.val(textbox.curval);
+  }
+
+  function evalCalc(){
+    var result = eval(textbox.lastval + textbox.operation + textbox.curval);
+    textbox.val(result);
+    textbox.lastval = result;
+    textbox.curval = "";
+  }
+
+  resetCalc();
+  console.log("ready");
 
   $(".number").click(function(){
-    console.log("number");
+    if (textbox.curval === "0") {
+      textbox.curval = "";
+    }
+    textbox.curval += $(this).val();
+    textbox.val(textbox.curval);
+    console.log(textbox);
   });
   $(".basiccalc").click(function(){
-    console.log("basiccalc");
+    if (textbox.lastval === ""){
+      textbox.operation = $(this).val();
+      textbox.lastval = textbox.curval;
+      textbox.curval = "";
+    } else {
+      evalCalc();
+      textbox.operation = $(this).val();
+    }
+    console.log(textbox);
   });
   $(".AC").click(function(){
-    console.log("AC");
+    resetCalc();
+    console.log("PM");
   });
   $(".PM").click(function(){
+    //TODO: Implement inverter; how does he work after all?
     console.log("PM");
   });
   $(".decimal").click(function(){
-    console.log("decimal");
+    if (checkDecimal() === true){
+      textbox.curval += $(this).val();
+      textbox.val(textbox.curval);
+    }
   });
   $(".eval").click(function(){
-    console.log("eval");
+    evalCalc();
+    console.log(textbox);
   });
 });
